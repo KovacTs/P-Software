078ff0b520657cf7df8998d8b29a049a1fbeb04f
KovacTs <fernandoe.llancao@gmail.com>
2024-10-09 01:49:10 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Ripley',
+        productNumber: '50000',
+        paymentStatus: 'Gasto',
+        status: 'Banco Santander'
+    },
+    {
+        productName: 'Paris',
+        productNumber: '94990',
+        paymentStatus: 'Gasto',
+        status: 'Banco Santander'
+    },
+    {
+        productName: 'Sueldo',
+        productNumber: '700000',
+        paymentStatus: 'Ingreso',
+        status: 'Banco Santander'
+    },
+]
